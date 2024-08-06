f83b26e7af2f8c88330b4f99499b4acc48dff4fa
Orekhova <irinamarkina803@gmail.com>
2024-08-06 10:31:52 +0400

    Реализовано Задание 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,16 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [-b / (2 * a)];
+  } else if (d > 0) {
+    arr = [-b + Math.sqrt(d), -b - Math.sqrt(d)];
+  }
   return arr;
 }
+console.log(solveEquation());
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {}
