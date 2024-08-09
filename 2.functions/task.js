c93818b8571d0b61c09e28e86f19953a7641e49c
Orekhova <irinamarkina803@gmail.com>
2024-08-09 11:28:23 +0400

    Hometask 2: Task1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,39 @@
-function getArrayParams(...arr) {
+// function getArrayParams(...arr) {
 
-  return { min: min, max: max, avg: avg };
-}
+//   return { min: min, max: max, avg: avg };
+// }
 
-function summElementsWorker(...arr) {
+const arr = [];
+const getArrayParams = function (...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg;
 
-}
+  for (let i = 0; i < arr.length; i++) {
+    const currentArrPosition = arr[i];
+    if (currentArrPosition > max) max = currentArrPosition;
+    if (currentArrPosition < min) min = currentArrPosition;
+    sum += arr[i];
+    avg = Number((sum / arr.length).toFixed(2));
+  }
 
-function differenceMaxMinWorker(...arr) {
+  const result = {
+    min: min,
+    max: max,
+    avg: avg,
+  };
+  return result;
+};
 
-}
+console.log(getArrayParams(-99, 99, 10));
 
-function differenceEvenOddWorker(...arr) {
+function summElementsWorker(...arr) {}
 
-}
+function differenceMaxMinWorker(...arr) {}
 
-function averageEvenElementsWorker(...arr) {
+function differenceEvenOddWorker(...arr) {}
 
-}
+function averageEvenElementsWorker(...arr) {}
 
-function makeWork (arrOfArr, func) {
-
-}
+function makeWork(arrOfArr, func) {}
