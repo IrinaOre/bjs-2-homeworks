1bdc0cfa3ce649e2c397f35b7be1f473930a74ab
Orekhova <irinamarkina803@gmail.com>
2024-08-24 19:54:15 +0400

    Homework: objects changed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -4,29 +4,27 @@ function Student(name, gender, age) {
   this.age = age;
   this.marks = [];
 }
-let student1 = new Student();
-let student2 = new Student();
+let student1 = new Student("Ann", "женский", 19);
+let student2 = new Student("Артём", "мужской", 25);
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 };
 
 Student.prototype.addMarks = function (...marksToAdd) {
-  if (this.marks.length === 0 && this.marks === false) {
-    return 0;
-  }
-  this.marks = marksToAdd;
+  this.marks.push(...marksToAdd);
 };
 
 Student.prototype.getAverage = function () {
-  if (this.marks.length === 0 && this.marks === false) {
+  if (this.marks in Student) {
+    let sum = 0;
+    for (let i = 0; i < this.marks.length; i++) {
+      sum += this.marks[i];
+    }
+    return sum / this.marks.length;
+  } else {
     return 0;
   }
-  let sum = 0;
-  for (let i = 0; i < this.marks.length; i++) {
-    sum += this.marks[i];
-  }
-  return sum / this.marks.length;
 };
 
 Student.prototype.exclude = function (reason) {
