76a48c7fe51307208edb33e20d8ce1aba1409af9
Orekhova <irinamarkina803@gmail.com>
2024-08-26 16:00:53 +0400

    Homework on 'Objects' changes

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,42 +1,3 @@
-// function Student(name, gender, age) {
-//   this.name = name;
-//   this.gender = gender;
-//   this.age = age;
-//   this.marks = [];
-// }
-// let student1 = new Student();
-// let student2 = new Student();
-
-// Student.prototype.setSubject = function (subjectName) {
-//   this.subject = subjectName;
-// };
-
-// Student.prototype.addMarks = function (...marksToAdd) {
-//   if ("marks" in this.marks) {
-//     this.marks.push(...marksToAdd);
-//   } else {
-//     return 0;
-//   }
-// };
-
-// Student.prototype.getAverage = function () {
-//   if (this.marks === true || this.marks.length === 0) {
-//     return 0;
-//   } else {
-//     let sum = 0;
-//     for (let i = 0; i < this.marks.length; i++) {
-//       sum += this.marks[i];
-//     }
-//     return sum / this.marks.length;
-//   }
-// };
-
-// Student.prototype.exclude = function (reason) {
-//   delete this.marks;
-//   delete this.subject;
-//   this.excluded = reason;
-// };
-
 function Student(name, gender, age) {
   this.name = name;
   this.gender = gender;
@@ -51,7 +12,7 @@ Student.prototype.setSubject = function (subjectName) {
 };
 
 Student.prototype.addMarks = function (...marksToAdd) {
-  if (!("marks" in this.marks)) {
+  if ("marks" in this.marks) {
     this.marks.push(...marksToAdd);
   } else {
     return 0;
@@ -59,7 +20,7 @@ Student.prototype.addMarks = function (...marksToAdd) {
 };
 
 Student.prototype.getAverage = function () {
-  if (!this.marks || this.marks.length === 0) {
+  if (this.marks === true || this.marks.length === 0) {
     return 0;
   } else {
     let sum = 0;
