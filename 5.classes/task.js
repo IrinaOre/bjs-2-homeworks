fd6bc593f4252e8f2196450058f1bbab58a042dd
Orekhova <irinamarkina803@gmail.com>
2024-08-30 09:04:19 +0400

    Classes 1

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -26,17 +26,6 @@ class PrintEditionItem {
   }
 }
 
-const sherlock = new PrintEditionItem(
-  "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
-  2019,
-  1008
-);
-
-console.log(sherlock.releaseDate); //2019
-console.log(sherlock.state); //100
-sherlock.fix();
-console.log(sherlock.state); //100
-
 class Magazine extends PrintEditionItem {
   constructor(name, releaseDate, pagesCount, state, type) {
     super(name, releaseDate, pagesCount, state, type);
@@ -70,15 +59,3 @@ class DetectiveBook extends Book {
     this.type = "detective";
   }
 }
-const picknick = new FantasticBook(
-  "Аркадий и Борис Стругацкие",
-  "Пикник на обочине",
-  1972,
-  168
-);
-
-console.log(picknick.author); //"Аркадий и Борис Стругацкие"
-picknick.state = 10;
-console.log(picknick.state); //10
-picknick.fix();
-console.log(picknick.state); //15
