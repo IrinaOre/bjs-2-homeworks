6f78acb15ccce73d1579296fb81d95de5cfd5053
Orekhova <irinamarkina803@gmail.com>
2024-09-08 07:26:42 +0400

    Error

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,15 @@
-﻿
+﻿const parseCount = function (valueToParse) {
+  if (isNaN(valueToParse)) {
+    throw new Error("Невалидное значение");
+  } else {
+    return Number.parseFloat(valueToParse);
+  }
+};
+
+const validateCount = function (valueToParse) {
+  try {
+    return parseCount(valueToParse);
+  } catch (error) {
+    console.log(error);
+  }
+};
