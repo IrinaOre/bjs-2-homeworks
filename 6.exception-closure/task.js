705f2d30adc60554e6dcba8acd3b3f48845f8832
Orekhova <irinamarkina803@gmail.com>
2024-09-12 12:42:33 +0400

    Exceptions changes

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -31,10 +31,9 @@ class Triangle {
 
   get area() {
     let p = this.perimeter * 0.5;
-    let s = Number(
+    return Number(
       Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))
     ).toFixed(3);
-    return s;
   }
 }
 
